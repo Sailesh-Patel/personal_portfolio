e4630155c8fbe2e3ffee321905cdd0d4e97f1f1a
Sailesh-Patel <150167583+Sailesh-Patel@users.noreply.github.com>
2024-03-16 13:51:32 +0000

    asset images moved to public folder to access

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import React, {useState } from "react";
-import  "./Navbar.css";
-import {getImageUrl} from "../../utils";
+import React, { useState } from "react";
+import "./Navbar.css";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return <nav className="navbar">
         <a className="title" href="/">Portfolio</a>
         <div className="menu">
-            <img className="menuBtn" src={getImageUrl("navbar/menuIcon.png")} alt="menu-button"></img>
-            <ul className="menuItems">
+            <img className="menuBtn" 
+            
+            src={menuOpen
+                ? "/assets/navbar/closeIcon.png"
+                : "/assets/navbar/menuIcon.png"      
+            }
+                alt="menu-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+                ></img>
+            <ul 
+            className="menuItems"
+            onClick={() => setMenuOpen(false)}
+            >
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#projects">Projects</a></li>
